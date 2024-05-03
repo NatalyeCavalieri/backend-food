526b670b66bc8589d3e7b1d00d84c4d244e32509
Natalye Cavalieri <cavalieridev@gmail.com>
2024-05-03 17:23:37 +0200

    init project

---
+++ src/providers/diskStorageDish.js
@@ -0,0 +1,26 @@
+const fs = require("fs")
+const path = require("path")
+const uploadConfigs = require("../configs/uploads")
+
+
+class DiskStorageDish {
+  async saveFile(file) {
+    await fs.promises.rename(
+      path.resolve(uploadConfigs.TMP_FOLDER, file),
+      path.resolve(uploadConfigs.UPLOADS_FOLDER_DISH, file)
+    )
+    return file
+  }
+
+  async deleteFile(file) {
+    const filePath = path.resolve(uploadConfigs.UPLOADS_FOLDER_DISH, file)
+    try {
+      await fs.promises.stat(filePath)
+    } catch {
+      return
+    }
+    await fs.promises.unlink(filePath)
+  }
+}
+
+module.exports = DiskStorageDish
---
+++ src/routes/users.routes.js
@@ -0,0 +1,47 @@
+const { Router } = require("express")
+const usersRoutes = Router()
+
+const confirmPassword = require("../middlewares/confirmPassword")
+
+const verifyFieldsIfEmpty = require("../middlewares/verifyFieldsIfEmpty")
+
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+
+const verifyUserAuthorization = require("../middlewares/verifyUserAuthorization")
+
+const userControllers = require("../controllers/usersControllers")
+
+const avatarControllers = require("../controllers/avatarControllers")
+
+const multer = require("multer")
+const uploadConfig = require("../configs/uploads")
+const upload = multer(uploadConfig.MULTER)
+
+usersRoutes.post(
+  "/",
+  verifyFieldsIfEmpty,
+  confirmPassword,
+  userControllers.create
+)
+usersRoutes.get("/", ensureAuthenticated, userControllers.show)
+usersRoutes.get(
+  "/:user_id",
+  ensureAuthenticated,
+  verifyUserAuthorization(["admin"]),
+  userControllers.index
+)
+usersRoutes.put("/", ensureAuthenticated, userControllers.update)
+usersRoutes.delete(
+  "/",
+  ensureAuthenticated,
+  verifyUserAuthorization(["admin"]),
+  userControllers.delete
+)
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  avatarControllers.update
+)
+
+module.exports = usersRoutes
